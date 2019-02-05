fac7149cb65d027690fc8eb99c1226334402fe88
James <jamesdownslab@gmail.com>
2019-02-05 15:49:07 +0000

    Added centre of mass finding from slots

---
+++ balancer_slots.py
@@ -0,0 +1,110 @@
+import Shaker.stepper as stepper
+import Shaker.arduino as arduino
+import Shaker.find_slots as find_slots
+import Generic.camera as camera
+import Generic.images as images
+import numpy as np
+from scipy import ndimage
+from scipy import spatial as sp
+import cv2
+import time
+
+class Balancer:
+    """
+    Class to balance the shaker using stepper motors and the slots
+    """
+
+    def __init__(self, port=None, no_of_sides=None):
+        cam_num = camera.find_camera_number()
+        if port is None:
+            port = arduino.find_port()
+        if no_of_sides is None:
+            self.no_of_sides = int(input('Enter the number of sides'))
+        else:
+            self.no_of_sides = no_of_sides
+
+        self.ard = arduino.Arduino('/dev/'+port)
+        self.Stepper = stepper.Stepper(self.ard)
+        self.webcam = camera.Camera(cam_type='logitechHD1080p', cam_num=cam_num)
+        self.hex, slots, self.center, self.contours, im = find_slots.find_regions(self.webcam.single_pic_array(), 10, 180)
+        images.display(im)
+        self.mask, self.masks = self.create_masks(slots)
+
+    def create_masks(self, slots):
+        im = self.webcam.single_pic_array()
+        mask = np.zeros(np.shape(im)[:2])
+        masks = []
+        for slot, contour in zip(slots, self.contours):
+            single_mask = np.zeros(np.shape(im)[:2])
+            slot = slot.reshape(4, 1, 2)
+            cv2.fillPoly(mask, [slot],  1)
+            cv2.fillPoly(single_mask, [contour], 1)
+            masks.append(single_mask)
+        return mask.astype(np.uint8), np.array(masks).astype(np.uint8)
+
+    def balance(self):
+        balanced = False
+        while balanced is False:
+            im = self.webcam.single_pic_array()
+            center, im = self.find_center(im)
+            instruction = self.find_instruction(center)
+            im = self.annotate_frame(im, center, instruction)
+            cv2.imshow('', im)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                balanced = True
+
+    def find_center(self, im):
+        centers = []
+        for n in range(6):
+            masked = images.mask_img(~im[:, :, 0], self.masks[n])
+            masked = images.threshold(masked, 200, cv2.THRESH_TOZERO)
+            center = ndimage.measurements.center_of_mass(masked.transpose())
+            im = images.draw_circle(im, center[0], center[1], 5, color=images.PURPLE, thickness=-1)
+            centers.append(center)
+        centers = np.array(centers)
+        mean_center = np.mean(centers, axis=0)
+        return mean_center, im
+
+    def annotate_frame(self, im, center, instr):
+        if len(im.shape) == 2:
+            im = np.dstack((im, im, im))
+        im = images.draw_circle(im, center[0], center[1], 3, images.PINK)
+        im = images.draw_circle(im, self.center[0], self.center[1], 3, images.RED)
+        im = images.draw_polygon(im, self.hex, color=images.GREEN, thickness=3)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        im = cv2.putText(im, 'Tray Center', (10, 30), font, 1, images.RED, 2, cv2.LINE_AA)
+        im = cv2.putText(im, 'Particle Center', (10, 60), font, 1, images.PINK, 2, cv2.LINE_AA)
+        im = cv2.putText(im, 'Hexagon', (10, 90), font, 1, images.GREEN, 2, cv2.LINE_AA)
+        distance = sp.distance.pdist([center, self.center])
+        cv2.putText(im, 'Pixel distance : {:.3f}'.format(distance[0]), (10, 120),font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(im, instr, (10, 150), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        for i in range(6):
+            im = cv2.putText(im, str(i), (int(self.hex[i, 0]), int(self.hex[i, 1])), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        return im
+
+    def find_instruction(self, center):
+        distances = sp.distance.cdist(np.array([center]), self.hex)[0]
+        sort = np.argsort(distances)
+        closest = sort[0]
+
+        if closest == 0:
+            inst = 'Lower Motors 1 and 2'
+        elif closest == 1:
+            inst = 'Lower Motor 1'
+        elif closest == 2:
+            inst = 'Raise Motor 2'
+        elif closest == 3:
+            inst = 'Raise Motors 1 and 2'
+        elif closest == 4:
+            inst = 'Raise Motor 1'
+        else:
+            inst = 'Lower Motor 2'
+        return inst
+
+
+
+if __name__ == "__main__":
+    bal = Balancer(no_of_sides=6)
+    # bal.balance()
+    bal.balance()
--- find_slots.py
+++ find_slots.py
@@ -3,9 +3,11 @@ import numpy as np
 import cv2
 from math import pi
 import scipy.spatial as sp
+import matplotlib.pyplot as plt
 
 def find_regions(image, bottom, top):
-    # subtract red from blue
+    # Subtract red channel from blue to channel.
+    # Remove grays from top and bottom of image
     difference = image[:, :, 0] - image[:, :, 2]
     _, difference = cv2.threshold(difference, bottom, 255,
                                   type=cv2.THRESH_TOZERO)
@@ -13,27 +15,36 @@ def find_regions(image, bottom, top):
                                   type=cv2.THRESH_TOZERO_INV)
     binary = images.adaptive_threshold(difference, 131)
 
-    contours, hiererchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # Find contours and sort by area
+    contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     area = []
     for cnt in contours:
         area.append(cv2.contourArea(cnt))
     area_sort = np.argsort(area)
 
+    # Second biggest contour is the hexagonal boundary
+    # Find center of hexagon using circle
     hex = contours[area_sort[-2]]
+    image = cv2.drawContours(image, [hex], 0, (0, 255, 0), 2)
     (xc, yc), r = cv2.minEnclosingCircle(hex)
+    image = cv2.circle(image, (int(xc), int(yc)), int(r), (255, 0, 0), 2)
     hex = np.squeeze(hex)
 
-    angles = np.zeros(len(hex))
-    for i, p in enumerate(hex):
-        angles[i] = np.arctan2(p[0]-xc, p[1]-yc)
-    angles *= 180/pi
-
+    # Calculate the angles of all the contour points
+    angles = np.arctan2(hex[:, 1]-yc, hex[:, 0]-xc)
+    angles = angles * 180/pi
+    # Find distance between all contour points and the centre of the hex
     dists = sp.distance.cdist(np.array([[xc, yc]]), hex)
     dists = np.squeeze(dists)
+
+    # Find the furthest point in 6 60 degree segments which are the corners
     corners = []
     for segment in range(6):
-        index = np.nonzero(~((angles >= (-180 + 60 * segment)) * (
-                    angles < (-120 + 60 * segment))))
+        if segment != 5:
+            index = np.nonzero(~((angles >= (-150 + 60 * segment)) * (
+                        angles < (-90 + 60 * segment))))
+        else:
+            index = np.nonzero((angles >= -150) * (angles < 150))
         temp_dists = dists.copy()
         temp_dists[index] = 0
         sort_list = np.argsort(temp_dists)
@@ -43,18 +54,23 @@ def find_regions(image, bottom, top):
         center = (int(hex[corner, 0]), int(hex[corner, 1]))
         image = cv2.circle(image, center, int(4), (0, 255, 0), 2)
 
-    hex_corners = np.array(corners)
+    hex_corners = np.array(hex[corners])
 
+    # Slots should be the next 6 biggest contours
+    # Save the coordinates of the bounding boxes
     slot_corners = []
+    slot_hulls = []
     for n in np.arange(3, 9):
+        slot_hulls.append(cv2.convexHull(contours[area_sort[-n]]))
         rect = cv2.minAreaRect(contours[area_sort[-n]])
         box = cv2.boxPoints(rect)
         box = np.int0(box)
         slot_corners.append(box)
         image = cv2.drawContours(image, [box], 0, (0, 255, 0), 2)
 
-    images.display(image)
-    return hex_corners, np.array(slot_corners)
+    slot_corners = sort_slots(np.array(slot_corners))
+
+    return hex_corners, slot_corners, (xc, yc), slot_hulls, image
 
 
 def sort_slots(slots):
@@ -65,5 +81,7 @@ def sort_slots(slots):
 
 if __name__ == "__main__":
     image = images.load_img('frame.png')
-    hex_corners, slot_corners = find_regions(image, 10, 180)
-    slots = sort_slots(slot_corners)
+    hex, slots, center, im = find_regions(image, 10, 180)
+    print(hex.shape)
+    print(slots.shape)
+    images.display(im)
