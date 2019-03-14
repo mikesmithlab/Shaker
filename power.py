f75200c095365eb1663e9839d10208827cecc1eb
James <jamesdownslab@gmail.com>
2019-03-14 10:03:35 +0000

    Added speaker interface

--- power.py
+++ power.py
@@ -2,20 +2,24 @@ from Shaker import arduino
 import numpy as np
 import time
 
+SPEAKER = "/dev/serial/by-id/usb-Arduino_LLC_Arduino_Micro-if00"
+POWERSUPPLY = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_757353034313511092C1-if00"
+
 
 class PowerSupply:
 
-    def __init__(self, port, baudrate):
-        self.ard = arduino.Arduino(port=port, rate=baudrate)
+    def __init__(self):
+        self.ps_ard = arduino.Arduino(port=POWERSUPPLY, rate=115200, wait=False)
+        self.speaker = arduino.Arduino(port=SPEAKER, rate=115200, wait=False)
         self.start_serial()
 
     def start_serial(self):
-        self.ard.send_serial_line('t')
+        self.ps_ard.send_serial_line('t')
         time.sleep(0.1)
-        self.ard.send_serial_line('x')
+        self.ps_ard.send_serial_line('x')
 
     def camera_trigger(self):
-        self.ard.send_serial_line('')  # Need to find out command
+        self.ps_ard.send_serial_line('')  # Need to find out command
 
     def ramp(self, start, end, rate):
 
@@ -34,26 +38,26 @@ class PowerSupply:
 
     def init_duty(self, val):
         string = 'i{:03}'.format(val)
-        self.ard.send_serial_line(string)
+        self.ps_ard.send_serial_line(string)
+        self.speaker.send_serial_line(string[1:])
 
     def change_duty(self, val):
         string = 'd{:03}'.format(val)
-        self.ard.send_serial_line(string)
+        self.ps_ard.send_serial_line(string)
+        self.speaker.send_serial_line(string[1:])
 
 
 
 if __name__ == "__main__":
-    port = "/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_757353034313511092C1-if00"
-    rate = 115200
-    PS = PowerSupply(port, rate)
-    # PS.ramp(0, 40, 1)
+    PS = PowerSupply()
+    PS.ramp(0, 40, 1)
     s = time.time()
-    PS.init_duty(0)
-    time.sleep(3)
-    PS.change_duty(100)
-    time.sleep(3)
-    PS.change_duty(0)
-    time.sleep(3)
-    PS.init_duty(0)
-    print(time.time() - s)
-    PS.start_serial()
+    # PS.init_duty(0)
+    # time.sleep(3)
+    # PS.change_duty(100)
+    # time.sleep(3)
+    # PS.change_duty(0)
+    # time.sleep(3)
+    # PS.init_duty(0)
+    # print(time.time() - s)
+    # PS.start_serial()
