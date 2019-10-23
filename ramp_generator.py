23ebdc096af46da9ef1b8b9cda0abfd626e94dab
James <jamesdownslab@gmail.com>
2019-10-23 13:47:04 +0100

    added function to convert linear acceleration ramps to duty cycle values

---
+++ ramp_generator.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+duties = np.loadtxt("Shaker/duties_map.txt")
+accels = np.loadtxt("Shaker/acceleration_map_volts.txt")
+
+
+def get_ramp(start, end, steps):
+    if (start < accels.min()) or (start > accels.max()):
+        print("start out of bounds")
+        return 0
+    if (end < accels.min()) or (end > accels.max()):
+        print("end out of bounds")
+        return 0
+    accel_values = np.linspace(start, end, steps)
+    accel_args = [np.abs((accels - v)).argmin() for v in accel_values]
+    return duties[accel_args]
